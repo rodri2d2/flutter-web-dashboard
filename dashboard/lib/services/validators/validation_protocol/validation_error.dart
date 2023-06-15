2e340da98fb96fad0fee70dc7c7a5ae3dfbc23de
Rodrigo Candido <rodrigoacandido@gmail.com>
2023-06-15 20:47:14 +0200

    Create validator protocol and it results case as interfaces

---
+++ dashboard/lib/services/validators/validation_protocol/validation_error.dart
@@ -0,0 +1,23 @@
+import 'package:dashboard/services/validators/imports.dart';
+
+/// Represents a validation result with error.
+///
+/// This class inherits from `ValidationResult` and is used to represent a validation result with error.
+/// The generic type `T` is used to allow flexibility in validation results.
+///
+/// The `error` property contains the specific error object that occurred during validation.
+///
+/// E.G.:
+///
+/// ```
+/// final error = FieldError('Invalid input');
+/// final result = ValidationError<FieldError>(error);
+/// print(result.description());  // Output: 'Validation Error: Invalid input'
+/// ```
+class ValidationError<T> extends ValidationResult<T> {
+  final T error;
+  ValidationError(this.error);
+
+  @override
+  String description() => 'Validation Error: $error';
+}
