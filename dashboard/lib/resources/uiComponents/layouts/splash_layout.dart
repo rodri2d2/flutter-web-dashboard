4bcc4735bebee636ee632aa2425557a70609be2e
Rodrigo Candido <rodrigoacandido@gmail.com>
2023-06-17 21:34:07 +0200

    Create sort of loading view

---
+++ dashboard/lib/resources/uiComponents/layouts/splash_layout.dart
@@ -0,0 +1,20 @@
+import '../../../essencial_imports.dart';
+
+class SplashLayout extends StatelessWidget {
+  const SplashLayout({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          CircularProgressIndicator(),
+          SizedBox(height: 20),
+          Text('Cheking...')
+        ],
+      )),
+    );
+  }
+}
