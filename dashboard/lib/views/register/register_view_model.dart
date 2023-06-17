4909c7db3ff6cac47101dee633324f6d2f65083c
Rodrigo Candido <rodrigoacandido@gmail.com>
2023-06-17 11:20:47 +0200

    Create register view model and modify view to use new view model

---
+++ dashboard/lib/views/register/register_view_model.dart
@@ -0,0 +1,32 @@
+import 'package:dashboard/services/validators/name_validator.dart';
+
+import '../../essencial_imports.dart';
+import '../../services/validators/validation_imports.dart';
+import 'package:dashboard/services/validators/email_validator.dart';
+import 'package:dashboard/services/validators/password_validator.dart';
+
+class RegisterViewModel extends ChangeNotifier {
+  GlobalKey<FormState> formKey = GlobalKey<FormState>();
+  final Validator<String> emailValidator = EmailValidator();
+  final Validator<String> passwordValidator = PasswordValidator();
+  final Validator<String> nameValidator = NameValidator();
+
+  String email = '';
+  String password = '';
+  String name = '';
+
+  void validateForm() {
+    // Why I made this?
+    //
+    // Even though the validation is done externally, with this configuration,
+    // when you call validateForm, the validate method of FormState will traverse all the FormFields
+    // that are associated with the Form, call the validation functions you provided
+    // them with and will automatically display error messages in the user interface if the validation fails.
+    if (formKey.currentState!.validate()) {
+      print('Form validator...login');
+      print('$email === $password');
+    } else {
+      print('Form noy valid...login');
+    }
+  }
+}
