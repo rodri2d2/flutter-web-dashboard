eacbde2785a92e66487ff35c102b9af2404d9fd5
Rodrigo Candido <rodrigoacandido@gmail.com>
2023-06-19 16:18:37 +0200

    Add menu items to side bar

--- dashboard/lib/resources/resources_imports.dart
+++ dashboard/lib/resources/resources_imports.dart
@@ -3,3 +3,5 @@ export 'package:dashboard/resources/uiComponents/buttons/link_text.dart';
 export 'package:dashboard/resources/uiComponents/buttons/custom_outliined_button.dart';
 export 'package:dashboard/resources/uiComponents/navigation/sidebar.dart';
 export 'package:dashboard/resources/uiComponents/static_elements/logo.dart';
+export 'package:dashboard/resources/uiComponents/static_elements/text_separator.dart';
+export 'package:dashboard/resources/uiComponents/navigation/menu_item.dart';
--- dashboard/lib/resources/uiComponents/navigation/sidebar.dart
+++ dashboard/lib/resources/uiComponents/navigation/sidebar.dart
@@ -17,11 +17,67 @@ class SideBar extends StatelessWidget {
           const SizedBox(height: 50),
           const TextSeparator(text: 'Main'),
           MenuItem(
-            text: 'Dashboard',
-            icon: Icons.compass_calibration_outlined,
-            onpress: () => print('Dashboard item'),
-            isActive: false,
-          )
+              text: 'Dashboard',
+              icon: Icons.compass_calibration_outlined,
+              isActive: false,
+              onpress: () {}),
+          MenuItem(
+              text: 'Orders',
+              icon: Icons.shopping_cart_outlined,
+              isActive: false,
+              onpress: () {}),
+          MenuItem(
+              text: 'Analitics',
+              icon: Icons.show_chart_outlined,
+              isActive: false,
+              onpress: () {}),
+          MenuItem(
+              text: 'Cathegories',
+              icon: Icons.layers_outlined,
+              isActive: false,
+              onpress: () {}),
+          MenuItem(
+              text: 'Products',
+              icon: Icons.dashboard_outlined,
+              isActive: false,
+              onpress: () {}),
+          MenuItem(
+              text: 'Discount',
+              icon: Icons.attach_money_outlined,
+              isActive: false,
+              onpress: () {}),
+          MenuItem(
+              text: 'Customers',
+              icon: Icons.people_alt_outlined,
+              isActive: false,
+              onpress: () {}),
+          const SizedBox(height: 50),
+          const TextSeparator(text: 'UI Elements'),
+          MenuItem(
+              text: 'Icons',
+              icon: Icons.list_alt_outlined,
+              isActive: false,
+              onpress: () {}),
+          MenuItem(
+              text: 'Marketing',
+              icon: Icons.mark_email_read_outlined,
+              isActive: false,
+              onpress: () {}),
+          MenuItem(
+              text: 'Campaing',
+              icon: Icons.note_add_outlined,
+              isActive: false,
+              onpress: () {}),
+          MenuItem(
+              text: 'Black',
+              icon: Icons.post_add_outlined,
+              isActive: false,
+              onpress: () {}),
+          MenuItem(
+              text: 'Log out',
+              icon: Icons.exit_to_app_outlined,
+              isActive: false,
+              onpress: () {}),
         ],
       ),
     );
